22d0e2044ca1812c68a7b0618c77ecb6e27f829c
FernanMKL <mfernandito206@gmail.com>
2024-05-06 15:33:10 -0500

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Jugando a **play.panas.online**","Revisando servidor"];
 
 
 let currentIndex = 0;
